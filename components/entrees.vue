0790a0220991d7a91711fd9a069ed32eb339450b
dasyx <seanpnn00@gmail.com>
2022-12-23 12:33:09 +0100

    fix prix entrees

--- components/entrees.vue
+++ components/entrees.vue
@@ -7,22 +7,22 @@
           <li class="mb-2 flex items-center">
             <h4 class="text-lg font-bold w:mr-4">Soupe de Wookie</h4>
             <p class="text-white">Soupe de légumes et de viande, inspirée de l'univers de Star Wars</p>
-            <p class="text-white font-bold float-right">Prix : 10€</p>
+            <p class="text-white font-bold float-right">10€</p>
           </li>
           <li class="mb-2 flex items-center">
             <h4 class="text-lg font-bold">Bol de ramen de Naruto</h4>
             <p class="text-white">Bol de ramen au porc et aux œufs, inspiré de l'univers de Naruto</p>
-            <p class="text-white font-bold float-right">Prix : 12€</p>
+            <p class="text-white font-bold float-right">12€</p>
           </li>
           <li class="mb-2 flex items-center">
             <h4 class="text-lg font-bold">Feuilles de vigne farcies</h4>
             <p class="text-white">Feuilles de vigne farcies de riz, de viande hachée et d'épices, inspirées de l'univers de Game of Thrones</p>
-            <p class="text-white font-bold float-right">Prix : 8€</p>
+            <p class="text-white font-bold float-right">8€</p>
           </li>
           <li class="mb-2 flex items-center">
             <h4 class="text-lg font-bold">Nouilles sautées aux légumes</h4>
             <p class="text-white">Nouilles sautées aux légumes, inspirées de l'univers de The Witcher</p>
-            <p class="text-white font-bold float-right">Prix : 10€</p>
+            <p class="text-white font-bold float-right">10€</p>
           </li>
         </ul>
       </div>
