554b656e9eeeb83721ad909f6ac413a6c1d600dd
dasyx <seanpnn00@gmail.com>
2022-12-23 12:53:22 +0100

    fix md display

--- components/entrees.vue
+++ components/entrees.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="entrees container mx-auto p-4">
-    <div class="flex flex-col md:flex-row md:justify-center">
+    <div class="flex flex-col md:flex-row md:justify-around">
       <div class="w-full md:w-3/4">
         <h2 class="text-xl font-bold mb-4">Entrées</h2>
         <ul>
-          <li class="mb-2 flex items-center">
-            <h4 class="text-lg font-bold w:mr-4">Soupe de Wookie</h4>
-            <p class="text-white">Soupe de légumes et de viande, inspirée de l'univers de Star Wars</p>
-            <p class="text-white font-bold float-right">10€</p>
+          <li class="mb-2 flex">
+            <h4 class="text-lg w-1/3 font-bold w:mr-6 md:mr-6">Soupe de Wookie</h4>
+            <p class="text-white w-2/3 md:ml-4">Soupe de légumes et de viande, inspirée de l'univers de Star Wars</p>
+            <p class="text-white font-bold float-right md:ml-2">10€</p>
           </li>
-          <li class="mb-2 flex items-center">
-            <h4 class="text-lg font-bold">Bol de ramen de Naruto</h4>
+          <li class="mb-2 flex">
+            <h4 class="text-lg w-1/3 font-bold w:mr-4 md:mr-7">Bol de ramen de Naruto</h4>
             <p class="text-white">Bol de ramen au porc et aux œufs, inspiré de l'univers de Naruto</p>
             <p class="text-white font-bold float-right">12€</p>
           </li>
-          <li class="mb-2 flex items-center">
-            <h4 class="text-lg font-bold">Feuilles de vigne farcies</h4>
+          <li class="mb-2 flex">
+            <h4 class="text-lg w-1/2 font-bold w:mr-4 md:mr-9">Feuilles de vigne farcies</h4>
             <p class="text-white">Feuilles de vigne farcies de riz, de viande hachée et d'épices, inspirées de l'univers de Game of Thrones</p>
             <p class="text-white font-bold float-right">8€</p>
           </li>
-          <li class="mb-2 flex items-center">
-            <h4 class="text-lg font-bold">Nouilles sautées aux légumes</h4>
+          <li class="mb-2 flex">
+            <h4 class="text-lg w-1/3 font-bold w:mr-4 md:mr-9">Nouilles sautées aux légumes</h4>
             <p class="text-white">Nouilles sautées aux légumes, inspirées de l'univers de The Witcher</p>
             <p class="text-white font-bold float-right">10€</p>
           </li>
